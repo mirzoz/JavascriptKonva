f7b54e0484e71c2b31ffcf4927a71d505f049ccc
Marco Tzuc <marco_nazi45@hotmail.com>
2015-05-12 23:32:31 -0500

    creacionde 3 circulos

--- js/tarea3.js
+++ js/tarea3.js
@@ -7,3 +7,33 @@ var stage = new Konva.Stage({
 
 // creamos la capa
 var layer = new Konva.Layer();
+
+// creamos la forma
+var circle = new Konva.Circle({
+    x: stage.getWidth() / 2 - 50,
+    y: stage.getHeight() / 2 - 50,
+    radius: 7,
+    fill: 'black',
+    stroke: 'gray',
+    strokeWidth: 3
+});
+
+// añadimos una forma mas
+var circle2 = new Konva.Circle({
+    x: stage.getWidth() / 2 + 50,
+    y: stage.getHeight() / 2 + 50,
+    radius: 7,
+    fill: 'black',
+    stroke: 'gray',
+    strokeWidth: 3
+});
+
+//añadimos un circulo mas
+var circle3 = new Konva.Circle({
+    x: stage.getWidth() / 2,
+    y: stage.getHeight() / 2,
+    radius: 7,
+    fill: 'green',
+    stroke: 'yellow',
+    strokeWidth: 3
+});
